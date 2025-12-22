a48ba13ae8c1a4ef92fe2a303afee9a7169231f8
Jim Liaskos <jim.liaskos@gmail.com>
2025-12-22 09:16:33 +0200

    Move to lazy vim
     - Add colors
     - Add telescope
     - Add undotree

--- nvim/lua/dliaskos/init.lua
+++ nvim/lua/dliaskos/init.lua
@@ -2,6 +2,7 @@ require("dliaskos.set")
 require("dliaskos.git")
 require("dliaskos.remap")
 require("dliaskos.lsp")
+require("dliaskos.plugins")
 
 vim.diagnostic.config({
     virtual_text = true,
@@ -32,3 +33,9 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
