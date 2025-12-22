d59e34050ce82a3f6b7c48d1ce89766393a62661
Jim Liaskos <jim.liaskos@gmail.com>
2025-12-22 19:46:59 +0200

    Fix plugins file name

--- nvim/lua/dliaskos/init.lua
+++ nvim/lua/dliaskos/init.lua
@@ -33,9 +33,3 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
--- nvim/lua/dliaskos/plugins.lua
+++ nvim/lua/dliaskos/plugins.lua
@@ -1,8 +1,8 @@
 -- Telescope
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 vim.keymap.set('n', '<leader>pws', function()
@@ -15,11 +15,11 @@ vim.keymap.set('n', '<leader>pWs', function()
     builtin.grep_string({ search = word })
 end)
 
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>fg', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- UndoTree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
