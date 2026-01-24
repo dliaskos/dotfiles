11b9a8ade4be0a0f689c784a0eb9221af5fce423
Jim Liaskos <jim.liaskos@gmail.com>
2026-01-24 11:50:05 +0200

    Fine tune telescope search  - ignore ds_store  - ignore .git subfolder  - search in hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,35 +1,48 @@
 return {
-    'nvim-telescope/telescope.nvim',
+	"nvim-telescope/telescope.nvim",
 
-    tag = 'v0.2.0',
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 
-    dependencies = {
-        'nvim-lua/plenary.nvim'
-    },
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = {
+					"%.DS_Store",
+					"%.git/",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true, -- Keep this to see .env, .config, etc.
+				},
+			},
+		})
 
-    config = function()
-        require('telescope').setup({})
+		local builtin = require("telescope.builtin")
 
-        local builtin = require('telescope.builtin')
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.find_files()
+		end, {})
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
+		vim.keymap.set("n", "<leader>pws", function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
 
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
+		vim.keymap.set("n", "<leader>pWs", function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
 
-        vim.keymap.set('n', '<leader>fg', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+		vim.keymap.set("n", "<leader>fg", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
 
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	end,
 }
