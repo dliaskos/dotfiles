65fe02bebe14355860b6aa3f02baed84416b1a7d
Jim Liaskos <jim.liaskos@gmail.com>
2026-01-24 14:53:11 +0200

    Fix lsp lua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,17 +5,32 @@ return {
 		"mason-org/mason.nvim",
 		"mason-org/mason-lspconfig.nvim",
 
+		"seblyng/roslyn.nvim",
+
 		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
+
 
 		-- "j-hui/fidget.nvim",
 	},
 
 	config = function()
-		require("mason").setup()
+		require("mason").setup({
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
+			},
+		})
+
 		require("mason-lspconfig").setup({
 			ensure_installed = { "lua_ls", "gopls", "yamlls" },
+		})
+
+		require("roslyn").setup({
+			env = {
+				DOTNET_ROLL_FORWARD = "LatestMajor",
+			},
 		})
 
 		local cmp = require("cmp")
@@ -30,7 +45,7 @@ return {
 
 		cmp.setup({
 			mapping = cmp.mapping.preset.insert({
-				['<C-Space>'] = cmp.mapping.complete(),
+				["<C-Space>"] = cmp.mapping.complete(),
 				-- ['<C-e>'] = cmp.mapping.abort(),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
