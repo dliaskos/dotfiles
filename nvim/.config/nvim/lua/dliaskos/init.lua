02fb2c73c62156c1c85362a8102daa3b705dd812
Jim Liaskos <jim.liaskos@gmail.com>
2026-02-08 01:11:53 +0200

    Clear neovim config

--- nvim/.config/nvim/lua/dliaskos/init.lua
+++ nvim/.config/nvim/lua/dliaskos/init.lua
@@ -7,55 +7,15 @@ vim.diagnostic.config {
   severity_sort = true,
   float = { border = 'rounded', source = 'if_many' },
   underline = { severity = vim.diagnostic.severity.ERROR },
-
-  -- Can switch between these as you prefer
   virtual_text = true, -- Text shows up at the end of the line
-  virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
-
-  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
   jump = { float = true },
 }
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
-
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', "<leader>p", [["+p]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- local augroup = vim.api.nvim_create_augroup
--- local dliaskos = augroup('dliaskos', {})
-
--- local autocmd = vim.api.nvim_create_autocmd
-
--- autocmd('LspAttach', {
---     group = dliaskos,
---     callback = function(e)
---         local opts = { buffer = e.buf }
---
---         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
---
---         vim.keymap.set("n", "<leader>//", function() vim.lsp.buf.format() end, opts)
---         vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
---         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
---         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
---         vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
---         vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
---
---         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---         vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
---         vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, opts)
---     end
--- })
---
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,51 +1,18 @@
--- return { -- Highlight, edit, and navigate codes
---     'nvim-treesitter/nvim-treesitter',
---     lazy = false,
---     config = function()
---       local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
---       -- require('nvim-treesitter').install(filetypes)
---       require('nvim-treesitter').setup({
---
---       })
---       vim.api.nvim_create_autocmd('FileType', {
---         pattern = filetypes,
---         callback = function() vim.treesitter.start() end,
---       })
---     end,
---   }
---
 return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' }
+        local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc',
+            'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' }
 
         require("nvim-treesitter").setup({
-            -- A list of parser names, or "all"
-            install = filetypes,
+            ensure_installed = parsers
+        })
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            -- sync_install = false,
-            --
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            -- auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- using this option may slow down your editor, and you may see some duplicate highlights.
-                -- instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = parsers,
+            callback = function() vim.treesitter.start() end,
         })
     end
 }
