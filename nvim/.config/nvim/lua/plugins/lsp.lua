c5b0d00e233bb94b45fd875b65ce0162778662c7
Jim Liaskos <dliaskos@vivawallet.com>
2026-03-22 11:51:58 +0200

    Tmux && ghostty

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
         },
 
         'WhoIsSethDaniel/mason-tool-installer.nvim',
-        -- "seblyng/roslyn.nvim",
+        "seblyng/roslyn.nvim",
 
         -- Useful status updates for LSP.
         { 'j-hui/fidget.nvim', opts = {} },
@@ -36,9 +36,11 @@ return {
 
                 local opts = { buffer = event.buf }
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 
                 vim.keymap.set("n", "<leader>//", function() vim.lsp.buf.format() end, opts)
+                vim.keymap.set("v", "<leader>//", function() vim.lsp.buf.format() end, opts)
                 vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
                 vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
@@ -85,8 +87,8 @@ return {
         local ensure_installed = vim.tbl_keys(servers or {})
 
         vim.list_extend(ensure_installed, {
-            -- 'roslyn',
-            'csharp-language-server',
+            'roslyn',
+            -- 'csharp-language-server',
             'lua-language-server',
             'stylua',
         })
@@ -124,11 +126,11 @@ return {
             },
         })
         vim.lsp.enable 'lua_ls'
-        vim.lsp.enable 'csharp_ls'
-        -- require("roslyn").setup({
-        --     env = {
-        --         DOTNET_ROLL_FORWARD = "LatestMajor",
-        --     },
-        -- })
+        -- vim.lsp.enable 'csharp_ls'
+        require("roslyn").setup({
+            env = {
+                DOTNET_ROLL_FORWARD = "LatestMajor",
+            },
+        })
     end,
 }
