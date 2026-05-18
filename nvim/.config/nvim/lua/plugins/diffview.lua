e32c34d4c37a0b177e149deb7a4c3dd1ef4ff8a2
Jim Liaskos <dliaskos@vivawallet.com>
2026-05-18 16:00:15 +0300

    Diffview

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,33 +1,51 @@
+local toggleDiffview = function(rev)
+    local df = require("diffview")
+    local lib = require("diffview.lib")
+    local view = lib.get_current_view()
+
+    if view then
+        df.close()
+    else
+        if rev then
+            df.open(rev)
+        else
+            df.open()
+        end
+    end
+end
+
 return {
     "sindrets/diffview.nvim",
 
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+        local diffview = require("diffview")
+        vim.opt.fillchars:append({ diff = "╱" })
 
-    opts = {
-        enhanced_diff_hl = true,
-        view = {
-            merge_tool = {
-                layout = "diff3_mixed",
+        diffview.setup({
+            enhanced_diff_hl = true,
+            view = {
+                merge_tool = {
+                    layout = "diff3_mixed",
+                },
             },
-        },
-    },
+        })
+    end,
     keys = {
         {
-            "<leader>dv",
-            desc = "Toggle Diffview",
+            "<leader>dfl",
+            desc = "Toggle Diffview for local changes",
             function()
-                vim.opt.fillchars:append({ diff = "╱" })
-
-                local lib = require("diffview.lib")
-                local view = lib.get_current_view()
-
-                if view then
-                    require("diffview").close()
-                else
-                    require("diffview").open()
-                end
+                toggleDiffview()
+            end,
+        },
+        {
+            "<leader>dfr",
+            desc = "Toggle Diffview for remote changes",
+            function()
+                toggleDiffview("origin/current...HEAD")
             end,
         },
     },
